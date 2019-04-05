c2237a08d82cb36ae705a3166aba983c698b85ab
rolandgarceau <rolandrgarceau@gmail.com>
2019-04-05 12:56:40 -0400

    explained the async "API"

--- javascript-generators/README.md
+++ javascript-generators/README.md
@@ -1,7 +1,10 @@
 # JavaScript Generators
-
+Kyle's video found at:
 > [https://youtu.be/Zk_rX2n3Ml8](https://youtu.be/Zk_rX2n3Ml8)
 
 Install [Node.js](https://nodejs.org/).
 
 Then run `node index.js` to run the example.
+
+This example was from 3 years ago but still shows how simple the basic flow of
+yield and the use of .next() .
--- javascript-generators/index.js
+++ javascript-generators/index.js
@@ -1,19 +1,31 @@
-// function* bears () {
-//   var kind = yield 'grizzly'
-//   yield kind + ' polar'
-//   console.log('kind: ' + kind)
-//   return 'done'
-// }
-// var bear = bears()
-// console.log(bear.next().value)
-// console.log(bear.next('ferocious').value)
-// console.log(bear.next().value)
+/*
+video explains the basics yield and next useage with this  
+function* bears () {
+  var kind = yield 'grizzly'
+  yield kind + ' polar'
+  console.log('kind: ' + kind)
+  return 'done'
+}
+var bear = bears()
+console.log(bear.next().value)
+console.log(bear.next('ferocious').value)
+console.log(bear.next().value)
 
+we can twist the file input/output module to do async functionality
+
+Kyle refers to this generator function to the run method as an "API"
+which has a resume callback to continue on with execution
+*/
 var fs = require('fs')
 
 run(function* (resume) {
+  //normal use of fs 3rd argument being the callback with the contents
+  //var contents = yield fs.readFile('big.file', 'utf8', function(err,contents))
+  //the resume in place of the function withe yield assurse the return into var
   var contents = yield fs.readFile('big.file', 'utf8', resume)
+  //now this whole thing he says is an "API" that can be used in a sync fashion
   var uppercase = contents.toUpperCase()
+  //then allow the async call with yield
   yield fs.writeFile('uppercase.file', uppercase, resume)
   console.log('All done!')
 })
